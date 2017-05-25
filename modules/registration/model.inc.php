529bdf0c512fbc166cf2d7cb9762ac848799ce18
Dima Salamatov <sal.dima@mail.ru>
2017-05-25 22:26:47 +0300

    Fixed index.tpl

--- modules/registration/model.inc.php
+++ modules/registration/model.inc.php
@@ -5,7 +5,7 @@ class Registration_Model
         $registered_users = Core::$Db->Select('user_auth', 'email', array('email' => $item['email']));
         if (empty($registered_users)) {
             $cur_id = Core::$Db->Insert("user_auth", array('email' => $item['email'], 'password' => $item['password']));
-            Core::$Db->Insert('user_data', array('user_id' => $cur_id, 'name' => $item['name'], 'surname' => $item['password']));
+            Core::$Db->Insert('user_data', array('user_id' => $cur_id, 'name' => $item['name'], 'surname' => $item['surname']));
             return true;
         }
         return false;
