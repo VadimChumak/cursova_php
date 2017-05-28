5b60e19531f092af3d70a543b41e6baf2dccf72b
Dima Salamatov <sal.dima@mail.ru>
2017-05-28 19:54:09 +0300

    Added Hash password

--- modules/registration/model.inc.php
+++ modules/registration/model.inc.php
@@ -4,7 +4,7 @@ class Registration_Model
     public function AddUser($item) {
         $registered_users = Core::$Db->Select('user_auth', 'email', array('email' => $item['email']));
         if (empty($registered_users)) {
-            Core::$Db->Insert("user_auth", array('email' => $item['email'], 'password' => $item['password']));
+            Core::$Db->Insert("user_auth", array('email' => $item['email'], 'password' => md5($item['password'])));
             $id = Core::$Db->Select("user_auth", "id", array('email' => $item['email']));
             Core::$Db->Insert('user_data', array('user_id' => $id[0]['id'], 'name' => $item['name'], 'surname' => $item['surname']));
             $this->Authorise(array(
@@ -21,7 +21,7 @@ class Registration_Model
         $_SESSION['user'] = $item;
     }
     public function Login($item) {
-        $current_user = Core::$Db->Select('user_auth', 'id, email, password', array('email' => $item['email'], 'password' => $item['password']));
+        $current_user = Core::$Db->Select('user_auth', 'id, email, password', array('email' => $item['email'], 'password' => md5($item['password'])));
         if (!empty($current_user)) {
             $user_data = Core::$Db->Select('user_data', 'name, surname', array('user_id' => $current_user[0]['id']));
             $this->Authorise(array(
