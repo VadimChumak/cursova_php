047520fd3b81e5f2694954a12ff71408e8e98f47
Vadim Chumak <vadimka.199@yandex.ru>
2017-06-21 15:15:12 +0300

    post type validation

--- modules/news/controller.inc.php
+++ modules/news/controller.inc.php
@@ -28,63 +28,104 @@ class News_Controller
         }
     }
     public function GetAction() {
-        $from = $_POST['from'];
-        $pageType = $_POST['pageType'];
-        $owner = $_POST['owner'];
-        $model = new News_Model();
-        $newsList = $model->NewsList($from, $owner, $pageType);
-        $res = json_encode($newsList);
-        echo $res;
-        exit(); 
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $from = $_POST['from'];
+            $pageType = $_POST['pageType'];
+            $owner = $_POST['owner'];
+            $model = new News_Model();
+            $newsList = $model->NewsList($from, $owner, $pageType);
+            $res = json_encode($newsList);
+            echo $res;
+            exit(); 
+        }
+        else {
+            return array(
+            "Content"  => "Page not found!";
+            );
+        }
     }
 
     public function DeleteAction() {
-        $postId = $_POST['postId'];
-        $newsModel = new News_Model();
-        $newsModel->DeleteNews($postId);
-        exit();
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $postId = $_POST['postId'];
+            $newsModel = new News_Model();
+            $newsModel->DeleteNews($postId);
+            exit();
+        }
+        else {
+            return array(
+            "Content"  => "Page not found!";
+            );
+        }
     }
 
     public function LikeAction() {
-        $postId = $_POST['postId'];
-        $action = $_POST['action'];
-        $model = new News_Model();
-        $model->ToggleLike($postId, $action);
-        $likeCount = 0;
-        $likeCount = $model->LikeCount($postId);
-        echo($likeCount[0]['count']);
-        exit();
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $postId = $_POST['postId'];
+            $action = $_POST['action'];
+            $model = new News_Model();
+            $model->ToggleLike($postId, $action);
+            $likeCount = 0;
+            $likeCount = $model->LikeCount($postId);
+            echo($likeCount[0]['count']);
+            exit();
+        }
+        else {
+            return array(
+            "Content"  => "Page not found!";
+            );
+        }
     }
 
     public function AddcommentAction() {
-        
-        $postId = $_POST['postId'];
-        $text = $_POST['text'];
-        $replyId = null;
-        if(isset($_POST['reply_id'])) {
-            $replyId = $_POST['reply_id'];
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $postId = $_POST['postId'];
+            $text = $_POST['text'];
+            $replyId = null;
+            if(isset($_POST['reply_id'])) {
+                $replyId = $_POST['reply_id'];
+            }
+            $model = new News_Model();
+            $arrayForSave = $model->AddComment($postId, $text, $replyId);
+            $user = Core::$Db->SelectJoin("user_data", 'image, name, surname', array("user_id" => $_SESSION['user']['id']));
+            $arrayForSave['image'] = $user[0]['image'];
+            $arrayForSave['name'] = $user[0]['name'];
+            $arrayForSave['surname'] = $user[0]['surname'];
+            echo json_encode($arrayForSave);   
+            exit();
+            }
+        else {
+            return array(
+            "Content"  => "Page not found!";
+            );
         }
-        $model = new News_Model();
-        $arrayForSave = $model->AddComment($postId, $text, $replyId);
-        $user = Core::$Db->SelectJoin("user_data", 'image, name, surname', array("user_id" => $_SESSION['user']['id']));
-        $arrayForSave['image'] = $user[0]['image'];
-        $arrayForSave['name'] = $user[0]['name'];
-        $arrayForSave['surname'] = $user[0]['surname'];
-        echo json_encode($arrayForSave);   
-        exit();
     }
 
     public function GetcommentAction() {
-        $postId = $_POST['postId'];
-        $model = new News_Model();
-        $comments = $model->CommentList($postId);
-        echo json_encode($comments);
-        exit();
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $postId = $_POST['postId'];
+            $model = new News_Model();
+            $comments = $model->CommentList($postId);
+            echo json_encode($comments);
+            exit();
+        }
+        else {
+            return array(
+            "Content"  => "Page not found!";
+            );
+        }
     }
 
     public function DeletecommentAction() {
-        $commentId = $_POST['commentId'];
-        Core::$Db->DeleteById('comment', 'id', $commentId);
-        exit();
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $commentId = $_POST['commentId'];
+            Core::$Db->DeleteById('comment', 'id', $commentId);
+            exit();
+        }
+        else {
+            return array(
+            "Content"  => "Page not found!";
+            );
+        }
     }
  }
--- modules/notification/controller.inc.php
+++ modules/notification/controller.inc.php
@@ -24,22 +24,29 @@ class Notification_Controller{
     }
 
     public function CheckAction() {
-        session_write_close();
-        $limit = 360;
-        $time = time();
-        $user_id = $_SESSION['user']['id'];
-        $date = $_POST['date'];
-        set_time_limit($limit + 5);
-        $notificationModel = new Notification_Model();
-        while((time() - $time) < $limit ) {
-            $notification = $notificationModel->GetNew($user_id, $date);
-            if(!empty($notification)) {
-                $res = json_encode($notification);
-                echo $res;
-                exit();
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            session_write_close();
+            $limit = 360;
+            $time = time();
+            $user_id = $_SESSION['user']['id'];
+            $date = $_POST['date'];
+            set_time_limit($limit + 5);
+            $notificationModel = new Notification_Model();
+            while((time() - $time) < $limit ) {
+                $notification = $notificationModel->GetNew($user_id, $date);
+                if(!empty($notification)) {
+                    $res = json_encode($notification);
+                    echo $res;
+                    exit();
+                }
+                sleep(1);
             }
-            sleep(1);
+            exit();
         }
-        exit();
+        else {
+            return array(
+            "Content"  => "Page not found!";
+            );
+        }
     }
 }
