0b0ce506dd7e1bf87784dab1c2693c5a8d285913
Vadim Chumak <vadimka.199@yandex.ru>
2017-06-22 22:39:24 +0300

    sh

--- modules/groups/controller.inc.php
+++ modules/groups/controller.inc.php
@@ -123,10 +123,11 @@ class Groups_Controller
             'NewsSection' =>  $NewsView->GetNewsList($newsList),
             'PageOwnerId' => $groupId,
             'MessagesCount' => $chatModel->GetNewMessagesCount($_SESSION['user']['id']),
-            'NotificationsCount' => $modelNotifi->GetNewNotificationCount($_SESSION['user']['id']),
+            'NotificationsCount' => $modelNotifi->GetNewNotificationCount($_SESSION['user']['id'])
         );
         return array(
-            "Content"  => $userPage->GetUserPage($params)
+            "Content"  => $userPage->GetUserPage($params),
+            "Script" => "<script src='/Content/js/news.js'></script>"
         );
 
     }
